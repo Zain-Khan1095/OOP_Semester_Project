8cbf7a1f02291b43a611a57c15ff8b2d51e957bb
zain khan <zainmatto34@gmail.com>
2026-05-10 18:56:47 +0500

    Delete db/schema.sql

--- db/schema.sql
+++
@@ -1,8 +0,0 @@
--- Table Schema
-CREATE TABLE IF NOT EXISTS inventory (
-    id INTEGER PRIMARY KEY AUTOINCREMENT, 
-    name TEXT NOT NULL,                   
-    price REAL NOT NULL,                  
-    stock INTEGER NOT NULL,               
-    expiry_date TEXT                      
-);
